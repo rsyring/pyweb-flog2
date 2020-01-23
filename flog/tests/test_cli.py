fe70be4a77370493a46bce4d45ee616e1992d3c6
Randy Syring <randy@thesyrings.us>
2020-01-23 16:22:30 -0500

    better test examples

--- flog/tests/test_cli.py
+++ flog/tests/test_cli.py
@@ -1,16 +1,25 @@
 from unittest import mock
 
+import responses
+
 
 class TestCLI:
 
-    def test_hn_profile(self, app):
+    @responses.activate
+    def test_requests_mock(self, app):
+        responses.add(
+            responses.GET,
+            'https://hacker-news.firebaseio.com/v0/user/rsyring.json',
+            json={'karma': 394, 'submitted': [1, 2, 3]}
+        )
+
         runner = app.test_cli_runner()
 
         result = runner.invoke(args=['hn-profile', 'rsyring'], catch_exceptions=False)
-        assert 'HackerNews user rsyring has 98 submissions and 394 karma.\n' == result.output
+        assert 'HackerNews user rsyring has 3 submissions and 394 karma.\n' == result.output
 
     @mock.patch('flog.libs.hackernews.User.get_json', autospec=True, spec_set=True)
-    def test_hn_profile_mock(self, m_get_json, app):
+    def test_library_mock(self, m_get_json, app):
         m_get_json.return_value = {'karma': 123, 'submitted': [1, 2, 3]}
 
         runner = app.test_cli_runner()
