59a8454b352979d504806f3f2ad0f57ba1405f95
Randy Syring <randy@thesyrings.us>
2020-06-19 14:27:27 -0400

    setup db string in CI

--- flog-config.py
+++ flog-config.py
@@ -1,3 +1,5 @@
+import os
+
 db_uri = 'postgresql://postgres@localhost:54321/flog'
 
 
@@ -8,6 +10,9 @@ def development_config(app, config):
 
 
 def testing_config(app, config):
-    config['SQLALCHEMY_DATABASE_URI'] = db_uri + '_tests'
+    if 'CI' in os.environ:
+        config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/postgres'
+    else:
+        config['SQLALCHEMY_DATABASE_URI'] = db_uri + '_tests'
 
     return config
